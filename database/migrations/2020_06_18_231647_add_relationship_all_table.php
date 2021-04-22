f15013b49b64b1e5eeec0396979539dd98dca936
mlh-nghianbt <nghianbt@lifetimetech.vn>
2021-04-22 11:09:34 +0700

    fix db

--- database/migrations/2020_06_18_231647_add_relationship_all_table.php
+++ database/migrations/2020_06_18_231647_add_relationship_all_table.php
@@ -11,64 +11,66 @@ class AddRelationshipAllTable extends Migration
      *
      * @return void
      */
-    // public function up()
-    // {
+    public function up()
+    {
 
-    //     Schema::table('admins', function (Blueprint $table) {
-    //         $table->foreign('user_id')->references('id')->on('users');
-    //     });
-    //     Schema::table('suppliers', function (Blueprint $table) {
-    //         $table->foreign('user_id')->references('id')->on('users');
-    //     });
-    //     Schema::table('buyers', function (Blueprint $table) {
-    //         $table->foreign('user_id')->references('id')->on('users');
-    //     });
-    //     Schema::table('tickets', function (Blueprint $table) {
-    //         $table->foreign('buyer_id')->references('id')->on('buyers');
-    //         $table->foreign('event_id')->references('id')->on('events');
-    //     });
-    //     Schema::table('supplier_events', function (Blueprint $table) {
-    //         $table->foreign('supplier_id')->references('id')->on('suppliers');
-    //         $table->foreign('event_id')->references('id')->on('events');
-    //     });
-    //     Schema::table('comments', function (Blueprint $table) {
-    //         $table->foreign('event_id')->references('id')->on('events');
-    //         $table->foreign('buyer_id')->references('id')->on('buyers');
-    //     });
-    //     Schema::table('events', function (Blueprint $table) {
-    //         $table->foreign('coupon_id')->references('id')->on('coupons');
-    //     });
-    // }
+        Schema::table('admins', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+        Schema::table('buyers', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->foreign('buyer_id')->references('id')->on('buyers');
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+        Schema::table('supplier_events', function (Blueprint $table) {
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('buyer_id')->references('id')->on('buyers');
+        });
+        Schema::table('events', function (Blueprint $table) {
+            $table->foreign('coupon_id')->references('id')->on('coupons');
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
 
-    // /**
-    //  * Reverse the migrations.
-    //  *
-    //  * @return void
-    //  */
-    // public function down()
-    // {
-    //     Schema::table('events', function (Blueprint $table) {
-    //         $table->dropForeign(['coupon_id']);
-    //     });
-    //     Schema::table('admins', function (Blueprint $table) {
-    //         $table->dropForeign(['user_id']);
-    //     });
-    //     Schema::table('suppliers', function (Blueprint $table) {
-    //         $table->dropForeign(['user_id']);
-    //     });
-    //     Schema::table('buyers', function (Blueprint $table) {
-    //         $table->dropForeign(['user_id']);
-    //     });
-    //     Schema::table('supplier_staffs', function (Blueprint $table) {
-    //         $table->dropForeign(['supplier_id']);
-    //     });
-    //     Schema::table('tickets', function (Blueprint $table) {
-    //         $table->dropForeign(['buyer_id']);
-    //         $table->dropForeign(['coupon_id']);
-    //     });
-    //     Schema::table('supplier_events', function (Blueprint $table) {
-    //         $table->dropForeign(['supplier_id']);
-    //         $table->dropForeign(['event_id']);
-    //     });
-    // }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign(['coupon_id']);
+        });
+        Schema::table('admins', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('buyers', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('supplier_staffs', function (Blueprint $table) {
+            $table->dropForeign(['supplier_id']);
+        });
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['buyer_id']);
+            $table->dropForeign(['coupon_id']);
+        });
+        Schema::table('supplier_events', function (Blueprint $table) {
+            $table->dropForeign(['supplier_id']);
+            $table->dropForeign(['event_id']);
+        });
+    }
 }
